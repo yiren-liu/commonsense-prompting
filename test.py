7901e3f4733debdf693321ff30ca7a85ffe5dadc
Yiren Liu <yirenl2@illinois.edu>
2022-09-11 23:10:56 -0500

    fix logger;

--- test.py
+++ test.py
@@ -42,8 +42,9 @@ from utils.evaluation import summary, Metric
 from config import Args
 
 # Configs
+# logger
 logger = logging.getLogger(__name__)
-
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 def set_seed(args):
     random.seed(args.seed)
